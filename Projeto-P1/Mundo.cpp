43487a061e93b01734d17b3cc4da9a85abe461fe
guilh <guilh@DESKTOP-5O16B1A>
2018-03-30 09:15:44 -0300

    Melhorias no layout e código

--- Projeto-P1/Mundo.cpp
+++ Projeto-P1/Mundo.cpp
@@ -51,74 +51,9 @@ int Mundo::get_tamanho_y()
 }
 
 
-void Mundo::setMundo()
+void Mundo::resetMap()
 {
-	/*	Determina o mundo padrão com 
-		bordas (1)	*/
 
-	int i;
-	int j;
-
-	// mapa vazio (0)
-	for (i = 0; i < tamanho_x; i++)
-		for (j = 0; j < tamanho_y; j++)
-			mapa[i][j] = '0';
-
-	// bordas (1)
-	for (i = 0; i < tamanho_x; i++)
-	{
-		for (j = 0; j < tamanho_y; j++)
-		{
-			if (i == 0)								// primeira linha
-				mapa[i][j] = '1';					
-			if (i == (tamanho_x - 1))				// ultima linha
-				mapa[i][j] = '1';
-			if (j == 0)								// coluna esquerda
-				mapa[i][j] = '1';
-			if (j == (tamanho_y - 1))				// coluna direita
-				mapa[i][j] = '1';
-		}
-	}
-
-	// obstáculos (2)
-	setObstaculo(3, 3, 3, 4);
-}
-
-
-void Mundo::setObstaculo(int x, int y, int w, int h) 
-{
-	/*	Determina obstáculos (2) 
-		Posicao obstáculo = x, y
-	    Tamanho dos obstaculos = w, h	*/
-
-	for (int i = 0; i < tamanho_x; i++)
-	{
-		for (int j = 0; j < tamanho_y; j++)
-		{
-			// obstáculo 1 na posição x, y com tamanho w1, h1
-			if (i == x && j == y)
-			{
-				for (int k = 0; k < 3; k++)
-				{
-					for (int l = 0; l < 4; l++)
-						mapa[i + k][j + l] = '2';
-				}
-			}
-		}
-	}
-}
-
-
-void Mundo::setCursorPosition(int x, int y)
-{
-	/* Determina posição do cursor 
-	   x é coluna, y é linha. A origem (0,0) 
-	   é o canto superior esquerdo.	*/
-	
-	static const HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
-	std::cout.flush();
-	COORD coord = { (SHORT)x, (SHORT)y };
-	SetConsoleCursorPosition(hOut, coord);
 }
 
 
@@ -129,19 +64,19 @@ void Mundo::setVeiculos(Carro *car, Caminhao *truck, Moto *bike)
 	if ((car->getX() >= 0 && car->getX() <= tamanho_x) &&
 		(car->getY() >= 0 && car->getY() <= tamanho_y))
 	{
-		mapa[car->getX()][car->getY()] = '%';
+		map[car->getX()][car->getY()] = '%';
 	}
-	
+
 	if ((truck->getX() >= 0 && truck->getX() <= tamanho_x) &&
 		(truck->getY() >= 0 && truck->getY() <= tamanho_y))
 	{
-		mapa[truck->getX()][truck->getY()] = '@';
+		map[truck->getX()][truck->getY()] = '@';
 	}
 
 	if ((bike->getX() >= 0 && bike->getX() <= tamanho_x) &&
 		(bike->getY() >= 0 && bike->getY() <= tamanho_y))
 	{
-		mapa[bike->getX()][bike->getY()] = '+';
+		map[bike->getX()][bike->getY()] = '+';
 	}
 }
 
@@ -170,38 +105,38 @@ void Mundo::printMundo()
 	{
 		cout << "      ";
 
-		for (int j = 0; j < tamanho_y; j++) 
+		for (int j = 0; j < tamanho_y; j++)
 		{
-			if (mapa[i][j] == '0')
+			if (map[i][j] == '0')
 			{
 				// determina cor do texto no console, e imprime char
 				SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), BLACK);
-				cout << mapa[i][j];
+				cout << map[i][j];
 			}
-			else if (mapa[i][j] == '2')
+			else if (map[i][j] == '2')
 			{
 				SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), RED);
-				cout << mapa[i][j];
+				cout << map[i][j];
 			}
-			else if (mapa[i][j] == '%')
+			else if (map[i][j] == '%')
 			{
 				SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), BLUE);
-				cout << mapa[i][j];
+				cout << map[i][j];
 			}
-			else if (mapa[i][j] == '@')
+			else if (map[i][j] == '@')
 			{
 				SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), YELLOW);
-				cout << mapa[i][j];
+				cout << map[i][j];
 			}
-			else if (mapa[i][j] == '+')
+			else if (map[i][j] == '+')
 			{
 				SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), MAGENTA);
-				cout << mapa[i][j];
+				cout << map[i][j];
 			}
 			else
 			{
 				SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), DARKGRAY);
-				cout << mapa[i][j];
+				cout << map[i][j];
 			}
 		}
 		cout << endl;
@@ -209,4 +144,88 @@ void Mundo::printMundo()
 
 	// retorna cursor para poisção inicial
 	setCursorPosition(0, 0);
+}
+
+
+void Mundo::setMundo()
+{
+	/*	Determina o mundo padrão com 
+		bordas (1)	*/
+
+	int i;
+	int j;
+
+	// mapa vazio (0)
+	for (i = 0; i < tamanho_x; i++)
+		for (j = 0; j < tamanho_y; j++)
+			map[i][j] = '0';
+
+	// bordas (1)
+	for (i = 0; i < tamanho_x; i++)
+	{
+		for (j = 0; j < tamanho_y; j++)
+		{
+			if (i == 0)								// primeira linha
+				map[i][j] = '1';					
+			if (i == (tamanho_x - 1))				// ultima linha
+				map[i][j] = '1';
+			if (j == 0)								// coluna esquerda
+				map[i][j] = '1';
+			if (j == (tamanho_y - 1))				// coluna direita
+				map[i][j] = '1';
+		}
+	}
+
+	// obstáculos (2)
+	setObstaculo(5, 12, 5, 10);
+	setObstaculo(5, 98, 5, 10);
+	setObstaculo(30, 12, 5, 10);
+	setObstaculo(30, 98, 5, 10);
+	setObstaculo(16, 50, 6, 18);
+
+	// copia para defaultMap
+	for (i = 0; i < tamanho_x; i++)
+	{
+		for (j = 0; j < tamanho_y; j++)
+		{
+			defaultMap[i][j] = map[i][j];
+		}
+	}
+}
+
+
+void Mundo::setObstaculo(int x, int y, int w, int h) 
+{
+	/*	Determina obstáculos (2) 
+		Posicao obstáculo = x, y
+	    Tamanho dos obstaculos = w, h	*/
+
+	for (int i = 0; i < tamanho_x; i++)
+	{
+		for (int j = 0; j < tamanho_y; j++)
+		{
+			// obstáculo 1 na posição x, y com tamanho w1, h1
+			if (i == x && j == y)
+			{
+				for (int k = 0; k < w; k++)
+				{
+					for (int l = 0; l < h; l++)
+						map[i + k][j + l] = '2';
+				}
+			}
+		}
+	}
+}
+
+
+void Mundo::setCursorPosition(int x, int y)
+{
+	/* Determina posição do cursor 
+	   x é coluna, y é linha. A origem (0,0) 
+	   é o canto superior esquerdo.	*/
+	
+	static const HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+	std::cout.flush();
+	COORD coord = { (SHORT)x, (SHORT)y };
+	SetConsoleCursorPosition(hOut, coord);
 }
--- Projeto-P1/Mundo.h
+++ Projeto-P1/Mundo.h
@@ -4,17 +4,19 @@ class Mundo
 public:
 	Mundo();
 	~Mundo();
-	void setMundo();
-	void setVeiculos(Carro *, Caminhao *, Moto *);
-	void movimenta(Carro *, Caminhao *, Moto *);
 	int get_tamanho_x();
 	int get_tamanho_y();
+	void resetMap();
+	void setVeiculos(Carro *, Caminhao *, Moto *);
+	void movimenta(Carro *, Caminhao *, Moto *);
 	void printMundo();
 private:
+	void setMundo();
 	void setObstaculo(int, int, int, int);
 	void setCursorPosition(int, int);
 	int tamanho_x;
 	int tamanho_y;
-	char mapa[40][120];
+	char map[40][120];
+	char defaultMap[40][120];
 };
 
